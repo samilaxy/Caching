ea9d279f0deb6d5795841b03071bc37af70672ad
Noye Samuel <59480282+samilaxy@users.noreply.github.com>
2023-03-28 09:19:11 +0000

    Initial Commit

---
+++ Caching/CachingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CachingApp.swift
+//  Caching
+//
+//  Created by Noye Samuel on 28/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct CachingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
