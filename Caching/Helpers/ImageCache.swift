bcc1de032bf6d74c7c2cd6a5b1de4298cef3c4d7
Noye Samuel <59480282+samilaxy@users.noreply.github.com>
2023-03-30 13:44:43 +0000

    update

--- Caching/Helpers/ImageCache.swift
+++ Caching/Helpers/ImageCache.swift
@@ -18,3 +18,29 @@ class ImageCache {
         set { newValue == nil ? cache.removeObject(forKey: NSString(string: key)) : cache.setObject(newValue!, forKey: NSString(string: key)) }
     }
 }
+
+class CacheManager {
+    static let shared = CacheManager()
+    private init() {}
+    var imageCache: NSCache<NSString, UIImage> = {
+        let cache = NSCache<NSString, UIImage>()
+        cache.countLimit = 100
+        cache.totalCostLimit = 1024 * 1024 * 100//  100mb
+        return cache
+    }()
+    
+    func addToCache(image: UIImage, name: String) {
+        imageCache.setObject(image, forKey: name as NSString)
+        print("Added to cache")
+    }
+    
+    func addFromCache( name: String) {
+        imageCache.removeObject(forKey: name as NSString)
+        print("Removed from cache")
+    }
+    
+    func getFromCache( name: String) -> UIImage? {
+       return  imageCache.object(forKey: name as NSString)
+        print("get from cache")
+    }
+}
--- Caching/View Models/ViewModel.swift
+++ Caching/View Models/ViewModel.swift
@@ -7,10 +7,12 @@
 
 import Foundation
 import Combine
+import UIKit
 
 class ViewModel: ObservableObject {
     
     private let cache = ImageCache.shared
+    private let cacheManager = CacheManager.shared
     @Published var images: [UnsplashImage] = []
     var networkManager = NetworkManager()
     
@@ -22,13 +24,24 @@ class ViewModel: ObservableObject {
     var cancellables = Set<AnyCancellable>()
     
     func getImages(){
+        
         guard images.isEmpty else { return }
         networkManager.getImages()
             .sink { _ in
                 
             }  receiveValue: { [weak self] returnedimages in
                 self?.images = returnedimages
+                self?.saveToChache(images: returnedimages)
             }
             .store(in: &cancellables)
+    }
+    
+    func saveToChache(images: [UnsplashImage]) {
+        var temImage: UIImage? = nil
+       
+        for image in images {
+            temImage = UIImage(named: image.urls.regular)
+            CacheManager.shared.addToCache(image: temImage, name: image.id)
+        }
     }
 }
