e82bf5078d776241936785e211f7069968cd84ab
Noye Samuel <59480282+samilaxy@users.noreply.github.com>
2023-03-31 10:19:05 +0000

    state restoration

--- Caching/Views/ImageView.swift
+++ Caching/Views/ImageView.swift
@@ -11,9 +11,11 @@ import Combine
     struct ImageView: View {
     
         @StateObject var imageLoader: ImageLoaderVM
+        @StateObject var stateManager: ImageViewStateManager
         
         init(imageUrl: String, key: String) {
             _imageLoader = StateObject(wrappedValue: ImageLoaderVM(url: URL(string: imageUrl)!, key: key))
+            _stateManager = StateObject(wrappedValue: ImageViewStateManager(url: URL(string: imageUrl), key: key))
         }
         
         var body: some View {
@@ -27,6 +29,11 @@ import Combine
             }
             .frame(height: 200)
             .padding(.all, 4)
+            .onDisappear {
+                    // Save state
+                stateManager.url = imageLoader.url
+                stateManager.key = imageLoader.imgKey
+            }
         }
     }
     
